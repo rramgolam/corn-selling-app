bd99a511f1663190669e3f75559a35c4af9cce0a
Rishi Ramgolam <rishi.ramgolam@madetech.com>
2021-03-03 14:09:01 +0000

    Test suite defined for tdd - failing tests, test coverage for upper, lower, extreme and valid cases

---
+++ test.js
@@ -0,0 +1,35 @@
+function testInvalidExtremeUpper() {
+	throw "Fail - libary func not support yet";
+}
+
+function testInvalidExtremeLower() {
+	throw "Fail - libary func not support yet";
+}
+
+function testValidExtremeUpper() {
+	throw "Fail - libary func not support yet";
+}
+
+function testValidExtremeLower() {
+	throw "Fail - libary func not support yet";
+}
+
+function testInvalidType() {
+	throw "Fail - libary func not support yet";
+}
+
+function testValid() {
+		throw "Fail - libary func not support yet";
+}
+
+
+function testAll(){
+	testInvalidExtremeUpper();
+	testInvalidExtremeLower();
+	testValidExtremeUpper();
+	testValidExtremeLower();
+	testInvalidType();
+	testValid();
+}
+
+testAll();
