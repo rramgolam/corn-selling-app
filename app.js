1ca7877bd507690c5d48ec5e35b4f879b578799d
Rishi Ramgolam <rishi.ramgolam@madetech.com>
2021-03-03 15:46:37 +0000

    Front end connected (plumbing), to now improve visuals

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var transportCostPerUnit = 0.25;
 
 function calculateTransportCost(numBags) {
-	
+
 	if (typeof numBags !== "number") {
 		throw "Invalid type exception";
 	}
@@ -12,3 +12,42 @@ function calculateTransportCost(numBags) {
 	return (numBags * transportCostPerUnit) * 2;
 
 }
+
+function onLoad() {
+	document.getElementById("bagInputField").value = 1;
+	updateDisplay(1);
+
+}
+
+function incrementNumBags() {
+
+	var current = document.getElementById("bagInputField");
+	var value = parseInt(current.value);
+
+	if (value + 1 <= 15) {
+		current.value = value + 1;
+
+		updateDisplay(value + 1);
+	}
+
+}
+
+function decrementNumBags() {
+
+	var current = document.getElementById("bagInputField");
+	var value = parseInt(current.value);
+
+	if (value - 1 >= 1) {
+		current.value = value - 1;
+
+		updateDisplay(value - 1);
+
+	}
+
+}
+
+function updateDisplay(value) {
+	document.getElementById("costDisplay").innerText = calculateTransportCost(value);
+}
+	
+
