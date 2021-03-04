ecf464a86c5fdfbfac6da02c989fa5a7f8f62dfe
Rishi Ramgolam <rishi.ramgolam@madetech.com>
2021-03-04 16:22:41 +0000

    Added ui elements for geese calculation

--- src/app.js
+++ src/app.js
@@ -61,9 +61,11 @@ function validateNumBagsInput(numBags, numGeese) {
 
 // entry point to app.js
 function onLoad() {
-	// default bag input field to 1
+	// default bag & geese input field to 1
 	document.getElementById("bagInputField").value = MINIMUM_BAGS_OF_CORN;
-	updateCost(MINIMUM_BAGS_OF_CORN);
+	document.getElementById("geeseInputField").value = MINIMUM_NUMBER_OF_GEESE;
+
+	updateElement(calculateTransportCost(MINIMUM_BAGS_OF_CORN, MINIMUM_NUMBER_OF_GEESE));
 }
 
 // increment bag input field - provided the value is within a valid range & update display
@@ -71,11 +73,21 @@ function incrementNumBags() {
 
 	var current = document.getElementById("bagInputField");
 	var value = parseInt(current.value);
+	value += 1;
 
-	if (value + 1 <= MAXIMUM_BAGS_OF_CORN) {
-		current.value = value + 1;
-		updateCost(value + 1);
+	try {
+		var geese = parseInt(document.getElementById("geeseInputField").value);
+		var result = calculateTransportCost(value, geese);
+		updateElement(result);
+		current.value = value;
+	} catch (err) {
+
 	}
+
+	// if (value + 1 <= MAXIMUM_BAGS_OF_CORN) {
+	// 	current.value = value + 1;
+	// 	updateCost(value + 1);
+	// }
 
 }
 
@@ -84,17 +96,75 @@ function decrementNumBags() {
 
 	var current = document.getElementById("bagInputField");
 	var value = parseInt(current.value);
+	value -= 1;
 
-	if (value - 1 >= MINIMUM_BAGS_OF_CORN) {
-		current.value = value - 1;
-		updateCost(value - 1);
+	try {
+		var geese = parseInt(document.getElementById("geeseInputField").value);
+		var result = calculateTransportCost(value, geese);
+		updateElement(result);
+		current.value = value;
+
+	} catch (err) {
+
 	}
 
+	// if (value - 1 >= MINIMUM_BAGS_OF_CORN) {
+	// 	current.value = value - 1;
+	// 	updateCost(value - 1);
+	// }
+
 }
 
-// update the current cost of transport & display
-function updateCost(value) {
-	document.getElementById("costDisplay").innerText = "£" + calculateTransportCost(value).toFixed(2);
+// increment bag input field - provided the value is within a valid range & update display
+function incrementNumGeese() {
+
+	var current = document.getElementById("geeseInputField");
+	var value = parseInt(current.value);
+	value += 1;
+
+	try {
+		var numBags = parseInt(document.getElementById("bagInputField").value);
+		var result = calculateTransportCost(numBags, value);
+		updateElement(result);
+		current.value = value;
+	} catch (err) {
+
+	}
+
+	// if (value + 1 <= MAXIMUM_BAGS_OF_CORN) {
+	// 	current.value = value + 1;
+	// 	updateCost(value + 1);
+	// }
+
 }
-	
+
+// decrement bag input field - provided the value is within a valid range & update display
+function decrementNumGeese() {
+
+	var current = document.getElementById("geeseInputField");
+	var value = parseInt(current.value);
+	value -= 1;
+
+	try {
+		var numBags = parseInt(document.getElementById("bagInputField").value);
+		var result = calculateTransportCost(numBags, value);
+		updateElement(result);
+		current.value = value;
+
+	} catch (err) {
+
+	}
+
+	// if (value - 1 >= MINIMUM_BAGS_OF_CORN) {
+	// 	current.value = value - 1;
+	// 	updateCost(value - 1);
+	// }
+
+}
+
+
+function updateElement(value) {
+		document.getElementById("costDisplay").innerText = "£" + value.toFixed(2);
+
+}
 
